135721fb2d852b9ee952cca3055a6900a70e20e4
Jenny Morgan <morganryan.j@northeastern.edu>
2025-05-20 19:18:29 -0700

    Author java file

---
+++ Labs/Lab1/src/main/java/example.java
@@ -0,0 +1,7 @@
+public class example {
+
+    public static void main(String[] args) {
+        Author author1 = new Author( "David Baldacci", "david@gmail.com", "Washington D.C.");
+        System.out.println(author1);
+    }
+}
