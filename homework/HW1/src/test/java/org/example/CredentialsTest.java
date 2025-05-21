94075706e916e99b3ed9b9396dc04d25dec2de7c
Jenny Morgan <morganryan.j@northeastern.edu>
2025-05-21 12:48:08 -0700

    Part 1 Complete

--- homework/HW1/src/test/java/org/example/CredentialsTest.java
+++ homework/HW1/src/test/java/org/example/CredentialsTest.java
@@ -10,23 +10,32 @@ class CredentialsTest {
 
     @BeforeEach
     void setUp() {
-        credentials = new Credentials();
+        credentials = new Credentials("Jenny", "Hello World");
     }
 
     @Test
     void getUsername() {
-        assert credentials.getUsername().getClass() == "String"
+        //testing to see if our getusername function returns a string
+        assertEquals(String.class, credentials.getUsername().getClass());
     }
 
     @Test
     void setUsername() {
+        //testing to see if we set out username correctly
+        credentials.setUsername("Jenny");
+        assertEquals("Jenny", credentials.getUsername());
     }
 
     @Test
     void getPasswordHash() {
+        //testing to see if our getter returns a string value
+        assertEquals(String.class, credentials.getPasswordHash().getClass());
     }
 
     @Test
     void setPasswordHash() {
+        //testing to see if when we set out password we saved our password correctly
+        credentials.setPasswordHash("Hello World");
+        assertEquals("Hello World", credentials.getPasswordHash());
     }
 }
