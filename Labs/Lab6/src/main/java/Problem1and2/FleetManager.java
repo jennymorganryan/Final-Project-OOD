f03b83d85b11e44e3f3673243d3f35535fd341cd
Jenny Morgan <morganryan.j@northeastern.edu>
2025-06-22 16:51:00 -0700

    lab6 done

--- Labs/Lab6/src/main/java/Problem1and2/FleetManager.java
+++ Labs/Lab6/src/main/java/Problem1and2/FleetManager.java
@@ -1,15 +1,47 @@
 package Problem1and2;
 
 public class FleetManager {
-    public TripReport drive (float distance, Vehicle vehicle) {
-        return new TripReport(vehicle, vehicle.getAvgSpeed(), distance, this.calculateDuration());
+
+    public TripReport drive(float distance, Vehicle vehicle) {
+        int duration = calculateDuration(distance, vehicle.getAvgSpeed());
+        return new TripReport(vehicle, vehicle.getAvgSpeed(), distance, duration);
+    }
+
+    public TripReport drive(int duration, Vehicle vehicle) {
+        float distance = calculateDistance(duration, vehicle.getAvgSpeed());
+        return new TripReport(vehicle, vehicle.getAvgSpeed(), distance, duration);
+    }
+
+    public TripReport drive(float distance, float speed, int duration, Vehicle vehicle) {
+        if (validateInputs(distance, speed, duration)) {
+            return new TripReport(vehicle, speed, distance, duration);
+        } else {
+            throw new IllegalArgumentException("Something went wrong with the provided inputs — laws of physics are not maintained");
+        }
+    }
+
+    public TripReport drive(float distance, float speed, Vehicle vehicle) {
+        float confirmedSpeed = confirmVehicleSpeed(speed, vehicle);
+        int duration = calculateDuration(distance, confirmedSpeed);
+        return new TripReport(vehicle, confirmedSpeed, distance, duration);
     }
 
     private int calculateDuration(float distance, float speed) {
-        if (speed == 0)
+        if (speed == 0) {
             throw new IllegalArgumentException("Speed cannot be zero");
-        else return Math.round(distance /speed);
+        }
+        return Math.round(distance / speed);
+    }
+
+    private float calculateDistance(int duration, float speed) {
+        return duration * speed;
+    }
+
+    private float confirmVehicleSpeed(float speed, Vehicle vehicle) {
+        return (vehicle.getMaxSpeed() >= speed) ? speed : vehicle.getMaxSpeed();
+    }
+
+    private Boolean validateInputs(float distance, float speed, int duration) {
+        return distance == duration * speed;
     }
 }
-
-
